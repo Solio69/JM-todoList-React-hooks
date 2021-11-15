5acc8279ff9660d9bdef1163398fbdf211e9c0ad
Anastasiya Kazanina <61517307+Solio69@users.noreply.github.com>
2021-11-15 20:14:08 +0300

    all components rewritten using hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,115 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import NewTaskForm from '../NewTaskForm/NewTaskForm';
 import TaskList from '../TaskList/TaskList';
 import Footer from '../Footer/Footer';
 
-export default class App extends Component {
-  state = {
-    todoData: JSON.parse(localStorage.getItem('todoData')) || [],
+const App = () => {
 
-    filter: JSON.parse(localStorage.getItem('filter')) || 'all', // all, active, !completed
-  };
+  // если в localStorage есть данные, то устанавливает значение из localStorage, если нет, то пустой массив
+  const [todoData, setTodoData]= useState(JSON.parse(localStorage.getItem('todoData')) || [])
 
-  // удаляет элемент списка
-  deleteItem = (id) => {
-    this.setState(({ todoData }) => {
-      const ind = todoData.findIndex((el) => el.id === id);
-      // eslint-disable-next-line id-length
-      const newArr = todoData.filter((_, index) => index !== ind);
+  // если в localStorage есть данные, то устанавливает значение из localStorage, если нет, то 'all'
+  const [filter, setFilter]= useState(JSON.parse(localStorage.getItem('filter')) || 'all')
 
-      return {
-        todoData: newArr,
-      };
-    });
-  };
-
-  // добавляет элемент в список
-  addItem = (text) => {
-    const newItem = this.createTodoItem(text);
-
-    this.setState(({ todoData }) => {
-      const newArr = [...todoData];
-      newArr.push(newItem);
-      return {
-        todoData: newArr,
-      };
-    });
-  };
-
-  // обновляет список с учетом выполненный tasks
-  onToggleDone = (id) => {
-    this.setState(({ todoData }) => ({
-      todoData: this.toggleProperties(todoData, id, 'completed'),
-    }));
-  };
-
-  // обновляет список с учетом радактируемых в данный момент tasks
-  onToggleEdit = (id) => {
-    this.setState(({ todoData }) => ({
-      todoData: this.toggleProperties(todoData, id, 'editing'),
-    }));
-  };
-
-  // обновляет свойства элемента
-  toggleProperties = (arr, id, propertyName) => {
-    const ind = arr.findIndex((el) => el.id === id);
-    const newArr = [...arr];
-    newArr[ind][propertyName] = !newArr[ind][propertyName];
-    return newArr;
-  };
-
-  // обновляет список с учетом отредактированный tasks
-  addEditedItem = (id, updateItem) => {
-    this.setState(({ todoData }) => {
-      const newArr = [...todoData];
-      const ind = newArr.findIndex((el) => el.id === id);
-      newArr[ind] = updateItem;
-      return {
-        todoData: newArr,
-      };
-    });
-  };
-
-  // изменяет значение свойства filter
-  onFilterChange = (filterName) => {
-    this.setState({ filter: filterName });
-  };
-
-  // удаляет все выполненные tasks
-  clearCompleted = () => {
-    this.setState(({ todoData }) => ({
-      todoData: [...todoData].filter((item) => !item.completed),
-    }));
-  };
-
-  filters = (arr, filter) => {
-    switch (filter) {
-      case 'all':
-        return arr;
-      case 'completed':
-        return arr.filter((item) => item.completed);
-      case 'active':
-        return arr.filter((item) => !item.completed);
-      default:
-        return arr;
-    }
-  };
-
-  changeTimer = (id, updatTimerData) => {
-    this.setState(({ todoData }) => {
-      const newArr = [...todoData];
-      const ind = newArr.findIndex((el) => el.id === id);
-      newArr[ind].timerData = updatTimerData;
-      return {
-        todoData: newArr,
-      };
-    });
-  };
-
-  // создает новый элемент списка
-  createTodoItem(label) {
+  const createTodoItem =(label) =>{
     const str = String(new Date());
 
     return {
@@ -127,42 +31,115 @@ export default class App extends Component {
     };
   }
 
-  render() {
-    const { todoData, filter } = this.state;
+  // добавляет элемент в список
+  const addItem = (text) => {
+    const newItem = createTodoItem(text);
+    const newArr = [...todoData];
+    newArr.push(newItem);
+    return setTodoData([...newArr])
+  };
 
-    // счетчик невыполненных задач
-    const tasksLeftCount = todoData.filter((el) => !el.completed).length;
+  // удаляет элемент списка
+  const deleteItem = (id) => {
+    const ind = todoData.findIndex((el) => el.id === id);
+    const newArr = todoData.filter((_, index) => index !== ind);
+    return setTodoData([...newArr])
+  };
 
-    // отфильтрованные элементы
-    const filtersItems = this.filters(todoData, filter);
+  // обновляет свойства элемента
+  const toggleProperties = (arr, id, propertyName) => {
+    const ind = arr.findIndex((el) => el.id === id);
+    const newArr = [...arr];
+    newArr[ind][propertyName] = !newArr[ind][propertyName];
+    return newArr;
+  };
 
-    // изменяет localStorage
-    localStorage.setItem('todoData', JSON.stringify(todoData));
-    localStorage.setItem('filter', JSON.stringify(filter));
+  // обновляет список с учетом выполненный tasks
+  const onToggleDone = (id) => {
+    setTodoData([...toggleProperties(todoData, id, 'completed')])
+  };
 
-    return (
-      <section className="todoapp">
+  // обновляет список с учетом отредактированный tasks
+  const addEditedItem = (id, updateItem) => {
+    const newArr = [...todoData];
+    const ind = newArr.findIndex((el) => el.id === id);
+    newArr[ind] = updateItem;
+    return setTodoData([...newArr])
+  };
+
+  // обновляет список с учетом радактируемых в данный момент tasks
+  const onToggleEdit = (id) => {
+    setTodoData([...toggleProperties(todoData, id, 'editing')])
+  };
+
+  const changeTimer = (id, updatTimerData) => {
+    const newArr = [...todoData];
+    const ind = newArr.findIndex((el) => el.id === id);
+    newArr[ind].timerData = updatTimerData;
+    return setTodoData([...newArr])
+      
+  };
+
+  // изменяет значение свойства filter
+  const onFilterChange = (filterName) => {
+    setFilter(filterName)
+  };
+
+  // удаляет все выполненные tasks
+  const clearCompleted = () => {
+    return setTodoData([...todoData].filter((item) => !item.completed))
+    
+  };
+
+  // фильтры 
+  const filters = (arr, filter) => {
+    switch (filter) {
+      case 'all':
+        return arr;
+      case 'completed':
+        return arr.filter((item) => item.completed);
+      case 'active':
+        return arr.filter((item) => !item.completed);
+      default:
+        return arr;
+    }
+  };
+
+  // отфильтрованные элементы
+  const filtersItems = filters(todoData, filter);
+
+  // счетчик невыполненных задач
+  const tasksLeftCount = todoData.filter((el) => !el.completed).length;
+
+  // изменяет localStorage
+  localStorage.setItem('todoData', JSON.stringify(todoData));
+  localStorage.setItem('filter', JSON.stringify(filter));
+
+  return(
+    <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
-          <NewTaskForm onItemAdded={this.addItem} />
+          <NewTaskForm 
+            onItemAdded={addItem}
+           />
         </header>
         <section className="main">
           <TaskList
             todos={filtersItems}
-            onDeleted={this.deleteItem}
-            onToggleDone={this.onToggleDone}
-            onToggleEdit={this.onToggleEdit}
-            addEditedItem={this.addEditedItem}
-            changeTimer={this.changeTimer}
+            onDeleted={deleteItem}
+            onToggleDone={onToggleDone}
+            onToggleEdit={onToggleEdit}
+            addEditedItem={addEditedItem}
+            changeTimer={changeTimer}
           />
           <Footer
             taskCount={tasksLeftCount}
             itemStatusFilter={filter}
-            onFilterChange={this.onFilterChange}
-            clearCompleted={this.clearCompleted}
+            onFilterChange={onFilterChange}
+            clearCompleted={clearCompleted}
           />
         </section>
       </section>
-    );
-  }
+  )
 }
+export default App;
